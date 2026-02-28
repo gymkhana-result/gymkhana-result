f476a6bd40c2e44656826ead01098d708793b466
gymkhana-result <bigwigs.hoopla-5m@icloud.com>
2026-02-28 09:52:35 +0900

    auto update (utf8 fixed)

--- watch.ps1
+++ watch.ps1
@@ -16,8 +16,11 @@ while ($true) {
 
         Write-Host "Change detected at $currentWrite"
 
-        # Default(=Shift_JIS)で読む
+        # Shift_JIS(Default)で読む
         $content = Get-Content $fullPath -Encoding Default
+
+        # charsetを書き換え
+        $content = $content -replace "charset=shift_jis", "charset=utf-8"
 
         # UTF8(BOM付き)で保存
         $utf8 = New-Object System.Text.UTF8Encoding $true
@@ -27,7 +30,7 @@ while ($true) {
 
         Set-Location $path
         git add .
-        git commit -m "auto update (utf8)"
+        git commit -m "auto update (utf8 fixed)"
         git push
 
         Write-Host "Converted to UTF8 & Pushed"
